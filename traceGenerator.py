59ac5e0bdde1839af3ca1fc5dbb19028e284387e
MukdenBaturu <jgaoln@bupt.edu.cn>
2022-09-05 21:09:05 +0800

    function_generator

--- traceGenerator.py
+++ traceGenerator.py
@@ -2,7 +2,16 @@ import numpy
 
 
 def func_generator (func_len, *func_val):
-    return 
+    lst=[]
+    if (len(func_val)==func_len):
+        for v in func_val:
+            lst.append(v)
+    else:
+        cnt=0
+        while cnt<func_len:
+            lst.append(func_val[cnt%len(func_val)])
+            cnt=cnt+1
+    return lst
 
 def traceGenrator(slot_len,interval,func_len):
     return
